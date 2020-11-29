8016b3f2607c0704fe61f3b074bb2e3dacf8a522
= <=>
2020-11-29 12:38:45 +0300

    pressure database from rsutido plot and plot examples

--- DataVizLec1.R
+++ DataVizLec1.R
@@ -1,1 +1,13 @@
-getwd()
+pressure
+
+plot(pressure$temperature, pressure$pressure, type = "l")
+
+library(ggplot2)
+qplot(pressure$temperature, pressure$pressure, geom = "line" )
+
+plot(pressure$temperature, pressure$pressure, type = "l")
+points(pressure$temperature, pressure$pressure)
+
+qplot(pressure$temperature, pressure$pressure, geom = "line" )
+points(pressure$temperature, pressure$pressure)
+
