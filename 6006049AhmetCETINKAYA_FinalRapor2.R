a67b1827ca53e5c8e8b6a95e53181917398d38af
= <=>
2021-01-28 22:41:27 +0300

    rapor 2 2 grafik hazır

--- 6006049AhmetCETINKAYA_FinalRapor2.R
+++ 6006049AhmetCETINKAYA_FinalRapor2.R
@@ -13,11 +13,15 @@ View(veriseti2)
 
 colnames(veriseti2)[] <- c("Dükkan", "Bölüm", "Tarih", "HaftalıkSatış", "Tatilmi")
 veriseti2$Dükkan <- as.character(veriseti2$Dükkan)
+veriseti2$Bölüm <- as.character(veriseti2$Bölüm)
+veriseti2$Tarih <- as.Date(veriseti2$Tarih, "%d/%m/%Y")
 View(veriseti2)
+str(veriseti2)
+
+yil2011_df <- filter(veriseti2, Tarih >="2011/01/01" & Tarih < "2012/01/01")
 
 
-
-yillik_toplam_liste <- veriseti2 %>%
+yillik_toplam_liste <- yil2011_df %>%
                     group_by(Dükkan) %>%
                     summarise(HaftalıkSatış = sum(HaftalıkSatış))
 
@@ -34,7 +38,41 @@ ggplot(yillik_dükkan_yuzde, aes(x = 2, y=prop, fill=Dükkan)) +
   geom_text(aes(y = ypos, label = round(prop,1)), color = "white", size=4) +
   scale_fill_brewer(palette="Set1")+
   xlim(0.5, 2.5) +
-  theme_void()
+  labs(
+       title = "2011 yılı Dükkan Satış Yüzdeleri",
+       subtitle = "2011 yılında dükkanların toplam satış payları",
+       caption = "Kaynak: https://www.kaggle.com/manjeetsingh/retaildataset") + 
+  theme_void() +
+  theme(
+    plot.title = element_text(color = "Blue",  face = "bold"),
+    plot.subtitle = element_text(color = "orange"),
+    plot.caption = element_text(color = "black", face = "italic")
+  )
+
+bolum2011 <- filter(veriseti2, Tarih >="2011/01/01" & Tarih < "2012/01/01")
+
+bolum2011_df <- bolum2011%>%
+                  group_by(Bölüm) %>%
+                  summarise(HaftalıkSatış = sum(HaftalıkSatış)) %>%
+                  top_n(10, HaftalıkSatış) %>%
+                  arrange(desc(HaftalıkSatış))
+
+ggplot(bolum2011_df, aes(HaftalıkSatış, Bölüm, label = round(HaftalıkSatış/1000000, 1))) +
+  geom_segment( aes(x=0, xend = HaftalıkSatış, y = Bölüm, yend=Bölüm)) +
+  geom_point(color = "red", size =3 ) +
+  geom_text(mapping = aes(label = round(HaftalıkSatış/1000000, 1)), position = position_dodge(0.9), hjust = -0.3, color="blue") +
+  scale_y_discrete(labels = abbreviate) +
+  scale_x_continuous(labels = scales::label_dollar(prefix="$"))+
+  labs(
+    title = "2011 yılı En Çok Hasılat Yapan Bölümler ",
+    subtitle = "2011 yılında en yüksek hasılatı olan bölüm numaraları",
+    caption = "Kaynak: https://www.kaggle.com/manjeetsingh/retaildataset")+
+  theme_light() +
+  theme(
+    plot.title = element_text(color = "Blue",  face = "bold"),
+    plot.subtitle = element_text(color = "orange"),
+    plot.caption = element_text(color = "black", face = "italic")
+  )
 
 
 
