0dd810d85ca9436b1055507953d4e8853d782840
= <=>
2021-01-30 10:40:59 +0300

    barchart, lollipop, donut line charts, filtering, sorting

--- 6006049AhmetCETINKAYA_FinalRapor1.R
+++ 6006049AhmetCETINKAYA_FinalRapor1.R
@@ -9,7 +9,7 @@ library(cowplot)
 library(ggpubr)
 library(ggmap)
 
-
+#veriyi al
 veriseti <- read.csv("TechStocks.csv")
 View(veriseti)
 
@@ -17,7 +17,7 @@ stock_verileri <- data.frame(veriseti$Date, veriseti$AAPL, veriseti$GOOG,veriset
 stock_verileri$veriseti.Date <- mdy(veriseti$Date)
 View(stock_verileri)
 colnames(stock_verileri)[] <- c('Tarih', 'APPLE', 'GOOGLE', 'MICROSOFT')
-#verilere göre tek tablo yap
+#veriye göre tek tablo yap
 melt_stocks <- reshape2::melt(stock_verileri, id.var = "Tarih")
 colnames(melt_stocks)[] <- c('Tarih', 'Firma', 'Değer')
 View(melt_stocks)
@@ -32,14 +32,14 @@ p_trend <- ggplot(melt_stocks, aes(x = Tarih, y = Değer, group = Firma, col = F
        title = "Apple, Google ve Microsoft Borsa Değerleri",
        subtitle = "01.12.2015-01.12.2017 Tarihleri Arası Veriler",
        caption = "Kaynak:vincentarelbundock.github.io/Rdatasets/doc/Stat2Data/TechStocks.html") +
-  theme_minimal_grid() +
-  theme(axis.text = element_text(size = 20),
-        axis.title = element_text(size = 20),
-        plot.title = element_text(color = "red", size = 20, face = "bold"),
-        plot.subtitle = element_text(color = "blue",size = 15),
-        plot.caption = element_text(color = "black", size =10, face = "italic"),
-        legend.title = element_text(size = 15),
-        legend.text = element_text(size = 10)) + 
+  theme_minimal_grid()  +
+  theme(axis.text = element_text(size = 15),
+        axis.title = element_text(size = 15),
+        plot.title = element_text(color = "red", size = 15, face = "bold"),
+        plot.subtitle = element_text(color = "blue",size = 10),
+        plot.caption = element_text(color = "black", size =6, face = "italic"),
+        legend.title = element_text(size = 10),
+        legend.text = element_text(size = 6))+
   theme(axis.text.x=element_text(angle=90, hjust=1)) 
 p_trend
 ggsave("APPLE_GOOGLE_MS.png")
@@ -67,9 +67,10 @@ yuzde_degisim$firma <- c("APPLE", "GOOGLE", "MICROSOFT")
 yuzde_degisim$oran <- c(apple_yuzde, google_yuzde, microsoft_yuzde)
 yuzde.df <- data.frame(yuzde_degisim)
 
-#line olarak göster
+#bar olarak göster
 p_yuzde <- ggplot(yuzde.df, aes(x = firma, y = oran)) +
-  geom_bar(stat = "identity", 
+  geom_bar(aes(reorder(firma,oran), oran),
+           stat = "identity", 
            fill = "cornflowerblue") +
   geom_text(mapping = aes(label = round(oran*100)), position = position_dodge(1), vjust = -0.2, size = 5,color="red")+
   scale_y_continuous(labels = scales::percent) +
@@ -79,15 +80,12 @@ p_yuzde <- ggplot(yuzde.df, aes(x = firma, y = oran)) +
        subtitle = "01.12.2015-01.12.2017 Yüzde Kar",
        caption = "Kaynak:vincentarelbundock.github.io/Rdatasets/doc/Stat2Data/TechStocks.html") +
   theme_minimal_grid() +
-  theme(axis.text = element_text(size = 20),
-        axis.title = element_text(size = 20),
-        plot.title = element_text(color = "red", size = 20, face = "bold"),
-        plot.subtitle = element_text(color = "blue",size = 15),
-        plot.caption = element_text(color = "black", size =10, face = "italic"),
-        legend.title = element_text(size = 15),
-        legend.text = element_text(size = 10))
+  theme(axis.text = element_text(size = 15),
+        axis.title = element_text(size = 15),
+        plot.title = element_text(color = "red", size = 15, face = "bold"),
+        plot.subtitle = element_text(color = "blue",size = 10),
+        plot.caption = element_text(color = "black", size =6, face = "italic"),
+        legend.title = element_text(size = 10),
+        legend.text = element_text(size = 6))
 p_yuzde
 ggsave("APPLE_GOOGLE_MS_yuzde.png")
-
-
-# tüm tabloları tek olarak hazırla   plot_grid(p_gt, p_tab,...)
--- 6006049AhmetCETINKAYA_FinalRapor2.R
+++ 6006049AhmetCETINKAYA_FinalRapor2.R
@@ -8,71 +8,82 @@ library(cowplot)
 library(ggpubr)
 library(ggmap)
 
+#veriyi al
 veriseti2 <- read.csv("sales-data-set.csv")
 View(veriseti2)
 
-colnames(veriseti2)[] <- c("Dükkan", "Bölüm", "Tarih", "HaftalıkSatış", "Tatilmi")
-veriseti2$Dükkan <- as.character(veriseti2$Dükkan)
+# veriyi düzenle
+colnames(veriseti2)[] <- c("Mağaza", "Bölüm", "Tarih", "HaftalıkSatış", "Tatilmi")
+veriseti2$Mağaza <- as.character(veriseti2$Mağaza)
 veriseti2$Bölüm <- as.character(veriseti2$Bölüm)
 veriseti2$Tarih <- as.Date(veriseti2$Tarih, "%d/%m/%Y")
 View(veriseti2)
 str(veriseti2)
 
+# ilgili yılı seç
 yil2011_df <- filter(veriseti2, Tarih >="2011/01/01" & Tarih < "2012/01/01")
 
-
+# Dükkanların yıllık paylarını hesapla
 yillik_toplam_liste <- yil2011_df %>%
-                    group_by(Dükkan) %>%
+                    group_by(Mağaza) %>%
                     summarise(HaftalıkSatış = sum(HaftalıkSatış))
 
 yillik_toplam <- data.frame(yillik_toplam_liste)
 
-yillik_dükkan_yuzde <- yillik_toplam %>% 
-  arrange(desc(Dükkan)) %>%
+yillik_magaza_yuzde <- yillik_toplam %>% 
+  arrange(desc(Mağaza)) %>%
   mutate(prop = HaftalıkSatış / sum(yillik_toplam$HaftalıkSatış) *100) %>%
   mutate(ypos = cumsum(prop)- 0.5*prop )
 
-ggplot(yillik_dükkan_yuzde, aes(x = 2, y=prop, fill=Dükkan)) +
+#donut graph ile çiz
+ggplot(yillik_magaza_yuzde, aes(x = 2, y=prop, fill=Mağaza)) +
   geom_bar(stat="identity", width=1, color="white") +
   coord_polar(theta = "y", start=0) +
   geom_text(aes(y = ypos, label = round(prop,1)), color = "white", size=4) +
   scale_fill_brewer(palette="Set1")+
   xlim(0.5, 2.5) +
   labs(
-       title = "2011 yılı Dükkan Satış Yüzdeleri",
-       subtitle = "2011 yılında dükkanların toplam satış payları",
+       title = "2011 yılı Mağaza Satış Yüzdeleri",
+       subtitle = "2011 yılında mağazaların toplam satış payları",
        caption = "Kaynak: https://www.kaggle.com/manjeetsingh/retaildataset") + 
   theme_void() +
   theme(
     plot.title = element_text(color = "Blue",  face = "bold"),
-    plot.subtitle = element_text(color = "orange"),
+    plot.subtitle = element_text(color = "red"),
     plot.caption = element_text(color = "black", face = "italic")
   )
 
+ggsave("2011 mağaza hasılat payları.png")
+
+# Bölümlerin yıllık hasılatı
 bolum2011 <- filter(veriseti2, Tarih >="2011/01/01" & Tarih < "2012/01/01")
 
+#En yüksek satış yapan 10 bölümü seç
 bolum2011_df <- bolum2011%>%
                   group_by(Bölüm) %>%
                   summarise(HaftalıkSatış = sum(HaftalıkSatış)) %>%
                   top_n(10, HaftalıkSatış) %>%
                   arrange(desc(HaftalıkSatış))
 
+# göster
 ggplot(bolum2011_df, aes(HaftalıkSatış, Bölüm, label = round(HaftalıkSatış/1000000, 1))) +
-  geom_segment( aes(x=0, xend = HaftalıkSatış, y = Bölüm, yend=Bölüm)) +
+  geom_segment( aes(x=0, xend = HaftalıkSatış, y = reorder(Bölüm, HaftalıkSatış), yend=Bölüm), color = "red") +
   geom_point(color = "red", size =3 ) +
   geom_text(mapping = aes(label = round(HaftalıkSatış/1000000, 1)), position = position_dodge(0.9), hjust = -0.3, color="blue") +
   scale_y_discrete(labels = abbreviate) +
-  scale_x_continuous(labels = scales::label_dollar(prefix="$"))+
-  labs(
+  scale_x_continuous(labels = scales::label_dollar(prefix="$", scale = 1e-6))+
+  coord_cartesian(xlim = c(0, 30000000)) +
+  labs( x = "Yıllık Satış Miktarı (Milyon USD)", y = "Bölüm Numarası",
     title = "2011 yılı En Çok Hasılat Yapan Bölümler ",
     subtitle = "2011 yılında en yüksek hasılatı olan bölüm numaraları",
     caption = "Kaynak: https://www.kaggle.com/manjeetsingh/retaildataset")+
   theme_light() +
   theme(
     plot.title = element_text(color = "Blue",  face = "bold"),
-    plot.subtitle = element_text(color = "orange"),
+    plot.subtitle = element_text(color = "red"),
     plot.caption = element_text(color = "black", face = "italic")
   )
 
-
+# kaydet
+ggsave("2011enyüksekhasılatbölümler.png")
 
